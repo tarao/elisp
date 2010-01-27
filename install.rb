7e620ebdd9c57966cc6bace14e882104bae2570e
tarao <tarao.gnn@gmail.com>
2010-01-27 16:40:53 +0000

    Update install script.

--- install.rb
+++ install.rb
@@ -1,34 +1,69 @@
 #! /usr/bin/env ruby
-Dir.chdir(File.dirname($0))
+require 'fileutils'
 
-install = '../site-lisp'
-compileonly = $*[0] == '--compile' && $*.shift
-
-dir = []
-if $*[0]
-  dir.push($*.shift)
-else
-  Dir.glob('*/'){|d| dir.push(d)}
+if $*.delete('-h') || $*.delete('--help')
+  print <<"EOM"
+Usage: #{$0} [OPTIONS] source...
+Options:
+  -p, --do-not-pull   Suppress pull action. (no)
+  -o, --compile-only  Suppress pull and link action. (no)
+  -c, --copy          Make copy instead of link. (no)
+  -v, --verbose       Ask when override. (no)
+  -s, --silent        Supress messages. (no)
+  --install=<dir>     Target directory. (../site-lisp)
+  --emacs=<bin>       Emacs binary path. (emacs)
+EOM
+  exit
 end
 
-dir.each do |d|
-  unless compileonly
-    puts("pull")
-    puts(`cd #{d}; git pull`)
-    puts("install from #{d}")
-  else
-    puts("compile for #{d}")
-  end
-  puts('')
+class Info
+  def initialize(silent, verbose) @s = silent; @v = verbose end
+  def puts(msg) Kernel.puts(msg) unless @s end
+  def ask(msg) return !@v || print(msg) || $stdin.gets =~ /^y/ end
+end
 
-  Dir.glob(File.join(d, '*.el')) do |f|
+dontpull = $*.delete('-p') || $*.delete('--do-not-pull')
+compileonly = $*.delete('-o') || $*.delete('--compile-only')
+copy = $*.delete('-c') || $*.delete('--copy')
+verbose = $*.delete('-v') || $*.delete('--verbose')
+s = $*.delete('-s') || $*.delete('--silent')
+verbose = verbose && !s
+info = Info.new(s, verbose)
+
+install = $*.reject!{|x|x=~/^--install=(.*)$/} && $1 || '../site-lisp'
+emacs = $*.reject!{|x|x=~/^--emacs=(.*)$/} && $1 || 'emacs'
+
+source = $*
+source.push('*/') if source.size < 1
+
+Dir.chdir(File.dirname($0))
+source.map!{|dir|Dir.glob(dir)}.each do |dir|
+  unless compileonly || dontpull
+    info.puts("pull")
+    info.puts(`cd #{dir}; git pull`)
+    info.puts("install from #{dir}")
+  end
+  info.puts('')
+
+  Dir.glob(File.join(dir, '*.el')) do |f|
     target = File.join(install, File.basename(f))
-    source = File.expand_path(f)
+    src = File.expand_path(f)
     unless compileonly
-      File.unlink(target) if File.exist?(target)
-      File.symlink(source, target)
-      puts("make link from #{source} to #{target}")
+      ovw_msg = proc{|name| "#{name} already exists. overwrite? (y/n) "}
+      write = [ target, target+'c' ].all? do |t|
+        !File.exist?(t) || info.ask(ovw_msg.call(t)) && File.unlink(t)
+      end
+      if write
+        if copy
+          info.puts("copy from #{source} to #{target}")
+          FileUtils.copy(src, target)
+        else
+          info.puts("make link from #{source} to #{target}")
+          File.symlink(src, target)
+        end
+      end
     end
-    puts(`emacs -batch -f batch-byte-compile #{target}`)
+    info.puts("compile for #{dir}")
+    info.puts(`#{emacs} -batch -f batch-byte-compile #{target}`)
   end
 end
